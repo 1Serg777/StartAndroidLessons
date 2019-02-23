3c8e0cd9e55ef86387c106a8d9569bf6c6d57fa3
Sergey Ageev <a.sergey9758@gmail.com>
2019-02-23 09:23:52 +0300

    Fragments commit

---
+++ p1061frgmtactvt/src/main/java/com/example/android/p1061frgmtactvt/Fragment2.java
@@ -0,0 +1,49 @@
+package com.example.android.p1061frgmtactvt;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Fragment2 extends Fragment {
+
+    final String LOG_TAG = "myLogs";
+
+    public interface onSomeEventListener {
+        public void someEvent(String s);
+    }
+
+    onSomeEventListener someEventListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            someEventListener = (onSomeEventListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement onSomeEventListener");
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment2, null);
+
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Button click in Fragment2");
+                someEventListener.someEvent("Test text to Fragment1");
+            }
+        });
+        return view;
+    }
+}
